0d3782c47b95c020ba21522144350b14e3b18d0f
DamSan04 <122906202+DamSan04@users.noreply.github.com>
2023-01-19 15:00:11 -0600

    Homework 1 Code File

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z;
+        Console.WriteLine($"X = {x}, Y = {y}");
+
+        z = (4*x*x)+(3*y);
+        Console.WriteLine($"The value of Z is: {z}");
+        
+
+
+    }
+}
