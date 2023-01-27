1c4756efd97061837f829abfb8943762640aeb44
DamSan04 <122906202+DamSan04@users.noreply.github.com>
2023-01-26 23:11:46 -0600

    Homework 2

---
+++ Homework2Q2.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework2Q2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input first num: ");
+        string  value1 = Console.ReadLine();
+        
+        int value1_int = Convert.ToInt16(value1);
+
+        Console.WriteLine("Please input second num: ");
+        string  value2 = Console.ReadLine();
+        int value2_int = Convert.ToInt16(value2);
+
+        Console.WriteLine("Please input third num: ");
+        string  value3 = Console.ReadLine();
+        int value3_int = Convert.ToInt16(value3);
+
+        if(value1_int<value2_int){
+            if(value1_int<value3_int){
+                Console.WriteLine($"The smallest value is: {value1_int}");
+            }
+        }
+        else{
+            if(value2_int<value3_int){
+                Console.WriteLine($"The smallest value is: {value2_int}");
+            }
+            if(value3_int<value2_int){
+                Console.WriteLine($"The smallest value is: {value3_int}");
+            }
+        }
+
+    }
+}
---
+++ HomeworkQ1.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Asking for letter grade input.
+        Console.WriteLine("Please input a letter grade: ");
+        string letter_grade = Console.ReadLine();
+        
+        if(letter_grade == "A" || letter_grade == "B" || letter_grade == "C" || letter_grade == "D" || letter_grade == "F"){
+            if(letter_grade == "A"){
+                Console.WriteLine("GPA Point: 4");
+
+            }
+            if(letter_grade == "B"){
+                Console.WriteLine("GPA Point: 3");
+
+            }
+            if(letter_grade == "C"){
+                Console.WriteLine("GPA Point: 2");
+
+            }
+            if(letter_grade == "D"){
+                Console.WriteLine("GPA Point: 1");
+
+            }
+            if(letter_grade == "F"){
+                Console.WriteLine("GPA Point: 0");
+
+            }
+
+        }
+        else{
+            Console.WriteLine("Wrong Letter Grade!");
+        } 
+    }
+}
